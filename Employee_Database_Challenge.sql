1e0853573f0ab3be4ada84ca41279d92b7e43859
EnderFreak <vkj1804@gmail.com>
2022-10-10 20:13:37 -0400

    Adding all necessary files for challenge 7

--- Employee_Database_Challenge.sql
+++ Employee_Database_Challenge.sql
@@ -33,4 +33,21 @@ FROM unique_titles as ut
 GROUP BY ut.title
 ORDER BY count DESC;
 
-
+--mentorship eligibility
+SELECT DISTINCT ON (em.emp_no)
+		em.emp_no,
+		em.first_name,
+		em.last_name,
+		em.birth_date,
+		de.from_date,
+		de.to_date,
+		ti.title
+INTO mentorship_eligibility
+FROM employees as em
+	INNER JOIN dept_emp as de
+	ON (em.emp_no = de.emp_no)
+	INNER JOIN titles as ti
+	ON (em.emp_no = ti.emp_no)
+WHERE (de.to_date = '9999-01-01')
+AND (em.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY em.emp_no
